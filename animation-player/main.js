8f1901c48df79c00e367a35f82385b470c7c2611
DzmitryKas <Kasperovich_dima@mail.ru>
2019-06-02 21:23:48 +0300

    fix: delete mistakes and add coments

--- animation-player/main.js
+++ animation-player/main.js
@@ -24,7 +24,7 @@ Array.from(elementChildrens).forEach(function(transform) {
 
 const chooseColor = document.querySelector('#choose-color');
 chooseColor.addEventListener('click', () => {
-  state.currentTool = 'chooseColor'; 
+  state.currentTool = 'chooseColor';
   console.log(state.currentTool);
 });
 
@@ -74,44 +74,47 @@ moveFigure.addEventListener('click', () => {
   state.currentTool = 'moveFigure';
 });
 
-    const draw = () => {
-        
-    let framesDom = document.querySelectorAll('#square');
-    let arrColor1 = [];
-    let arrColor2 = [];
-    let arrColor3 = [];
-    let arrColor = [arrColor1, arrColor2, arrColor3];
+// Создание массива цветов, и на их основе отрисовка кадра
+const draw = () => {
+  let framesDom = document.querySelectorAll('#square');
+  let arrColor1 = [];
+  let arrColor2 = [];
+  let arrColor3 = [];
+  let arrColor = [arrColor1, arrColor2, arrColor3];
 
-    framesDom.forEach(function(element, index) {
-    if (index < 3){
-    let color = getComputedStyle(element).backgroundColor;
-    arrColor1.push(color);}
-    if (index > 2 && index < 6){
-    let color = getComputedStyle(element).backgroundColor;
-    arrColor2.push(color);}
-    if (index > 5 && index < 9){
-    let color = getComputedStyle(element).backgroundColor;
-    arrColor3.push(color);}
-  })
+  framesDom.forEach(function(element, index) {
+    if (index < 3) {
+      let color = getComputedStyle(element).backgroundColor;
+      arrColor1.push(color);
+    }
 
-    let canvasMini = document.querySelectorAll('.canvas-mini');  
-    // if (canvas.getContext) {
-      let ctx = canvasMini[canvasMini.length - 1].getContext('2d');
+    if (index > 2 && index < 6) {
+      let color = getComputedStyle(element).backgroundColor;
+      arrColor2.push(color);
+    }
 
-      arrColor.forEach((row, m) => {
-        row.forEach((column, n) => {
-            ctx.fillStyle = column;
-            ctx.fillRect((n * 102), (m * 51), 100, 50);
-        })
-      })
-    // }
-    return arrColor
-  }
+    if (index > 5 && index < 9) {
+      let color = getComputedStyle(element).backgroundColor;
+      arrColor3.push(color);
+    }
+  });
 
+  let canvasMini = document.querySelectorAll('.canvas-mini');
+  let ctx = canvasMini[canvasMini.length - 1].getContext('2d');
+
+  arrColor.forEach((row, m) => {
+    row.forEach((column, n) => {
+      ctx.fillStyle = column;
+      ctx.fillRect(n * 102, m * 51, 100, 50);
+    });
+  });
+  return arrColor;
+};
+
+// Создание рамки для отрисовки, удаление кадра и создание копии
 function drawCanvas() {
-
-const menuFrame = document.querySelector('.menu-frame');
-const canvasWrapper = document.createElement('div');
+  const menuFrame = document.querySelector('.menu-frame');
+  const canvasWrapper = document.createElement('div');
   canvasWrapper.className = 'canvas-wrapper';
   menuFrame.insertBefore(canvasWrapper, menuFrame.children[menuFrame.children.length - 1]);
 
@@ -133,91 +136,87 @@ const canvasWrapper = document.createElement('div');
 
   const move = document.createElement('i');
   move.className = 'icon-braille';
-  canvasWrapper.appendChild(move);  
+  canvasWrapper.appendChild(move);
 
-  basket.addEventListener('click', () => {    
-    menuFrame.removeChild(canvasWrapper)  
-    numberFrame()  
+  basket.addEventListener('click', () => {
+    menuFrame.removeChild(canvasWrapper);
+    numberFrame();
   });
-  
-  doubleFile.addEventListener('click', () => { 
-    drawCanvas()
+
+  doubleFile.addEventListener('click', () => {
+    drawCanvas();
     const frame = draw();
-    frames.push(frame);    
-    numberFrame()
+    frames.push(frame);
+    numberFrame();
   });
-  
+
+  // Функция определения порядкового номера кадра
   function numberFrame() {
     const counter = document.querySelectorAll('.counter');
-    counter.forEach(function(el, index) {     
+    counter.forEach(function(el, index) {
       counter[index].innerText = index + 1;
     });
   }
-  numberFrame() 
-} 
-
-const drawAnimation = (arr) => {  
-    let canvasAnimation = document.querySelector('.canvas-animation');      
-        console.log('arr', arr);
-        //  if (canvas.getContext) {
-      let ctx = canvasAnimation.getContext('2d');
-
-      arr.forEach((row, m) => {
-        row.forEach((column, n) => {
-            ctx.fillStyle = column;
-            ctx.fillRect((n * 102), (m * 51), 100, 50);
-        })
-      })  
-    // }
-  }
-
-  function fun1() {
-    var rng=document.getElementById('r1'); //rng - это ползунок
-  var i1=document.getElementById('i1'); // i1 - input
-   i1.value = rng.value + ' fps';
-   return rng.value
+  numberFrame();
 }
 
+// Отрисовка кадров на холсте предварительного просмотра
+const drawAnimation = arr => {
+  let canvasAnimation = document.querySelector('.canvas-animation');
+  let ctx = canvasAnimation.getContext('2d');
 
-  let frameRate = 5;
-  let count = 1;
-const startAnimation = (frames) => {  
-       
-setInterval(() => {  
-    const frameRate = fun1();
-
-    if (count < frames.length) {
-        const frame = frames[count];   
-        drawAnimation(frame);
-        count++
-    }   
-    else {
-        count = 1;}    
-            
-    }, 1000/frameRate)
-}
-
-const resetCanvas = () => {
-    let colorSquare = document.querySelectorAll('#square');
-    colorSquare.forEach(function(el) {     
-    el.style.background = 'red';
+  arr.forEach((row, m) => {
+    row.forEach((column, n) => {
+      ctx.fillStyle = column;
+      ctx.fillRect(n * 102, m * 51, 100, 50);
     });
+  });
+};
+
+// Изменение FPS анимации
+function fun1() {
+  let rng = document.getElementById('r1');
+  let i1 = document.getElementById('i1');
+  i1.value = rng.value + ' fps';
+  return rng.value;
 }
 
-const frames = [];    
-document.querySelector('.add-frame').addEventListener('click', () => {      
- drawCanvas();
- const frame = draw();
- frames.push(frame); 
- console.log('frames', frames)
- resetCanvas (); 
- draw();
-  
-})
+let frameRate = 5;
+let count = 1;
+let rng = document.getElementById('r1');
 
- document.querySelector('.start').addEventListener('click', () => { 
-    startAnimation(frames)     
- })
+// Создание анимации на основе полученных кадров
+const startAnimation = frames => {
+  frameRate = fun1();
+  setInterval(() => {
+    if (count < frames.length) {
+      const frame = frames[count];
+      drawAnimation(frame);
+      count++;
+    } else {
+      count = 1;
+    }
+  }, 1000 / frameRate);
+};
 
+// Обновление холста
+const resetCanvas = () => {
+  let colorSquare = document.querySelectorAll('#square');
+  colorSquare.forEach(function(el) {
+    el.style.background = 'red';
+  });
+};
 
- 
+const frames = [];
+document.querySelector('.add-frame').addEventListener('click', () => {
+  drawCanvas();
+  const frame = draw();
+  frames.push(frame);
+  resetCanvas();
+  draw();
+});
+
+// Старт
+document.querySelector('.start').addEventListener('click', () => {
+  startAnimation(frames);
+});
