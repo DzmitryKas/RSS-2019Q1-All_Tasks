44799b50f9e3064da134e7ecc280f6537d3850d1
DzmitryKas <Kasperovich_dima@mail.ru>
2019-06-11 15:49:28 +0300

    feat: add field canvas, add pen and input color

--- piskel-clone/src/index.js
+++ piskel-clone/src/index.js
@@ -1,26 +1,55 @@
 import './app-view.css';
 import './reset.css';
 
+// отрисовка шахматной доски
 const canvasBasic = document.getElementById('canvas-basic');
-
 const ctx = canvasBasic.getContext('2d');
-console.log(ctx);
-for (let i = 0; i < 940; i += 4) {
-  for (let j = 0; i < 940; j += 4) {
-    if (j % 2 === 0) {
-      ctx.fillStyle = 'red';
-      ctx.fillRect(i, j, 4, 4);
-    }
+ctx.strokeRect(15, 15, 266, 266);
+ctx.strokeRect(18, 18, 260, 260);
+canvasBasic.width = 800;
+canvasBasic.height = 800;
+ctx.fillStyle = '#4c4c4c'; // меняем цвет клеток
+ctx.fillRect(0, 0, 800, 800);
+for (let i = 0; i < 128; i += 2) {
+  for (let j = 0; j < 128; j += 2) {
+    ctx.clearRect(i * 8, j * 8, 8, 8);
+    ctx.clearRect((i + 1) * 8, (j + 1) * 8, 8, 8);
   }
 }
 
-// arrColor.forEach((row, m) => {
-//   row.forEach((column, n) => {
-//     ctx.fillStyle = column;
-//     ctx.fillRect(n * 102, m * 51, 100, 50);
-//   });
-// });
 
 // document.querySelector('.pen').addEventListener((e) => {
+let myColor = 'red';
 
+// document.getElementById('colorrr').oninput = function () {
+// myColor = this.value;
+// ctx.beginPath();
+// };
+document.getElementsByClassName('first-current-color').oninput = function () {
+  console.log(this.value);
+   myColor = this.value;
+};
+
+canvasBasic.onmousedown = function (event) {
+  // let x = event.offsetX;
+  // let y = event.offsetY;
+  // ctx.strokeStyle = myColor;
+  // ctx.lineCap = 'round';
+  // ctx.lineWidth = 15;
+  // ctx.moveTo(x, y);
+  canvasBasic.onmousemove = function (event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x);
+    console.log(y);
+    ctx.fillRect(x - 5, y - 5, 25, 25);
+    ctx.fillStyle = myColor;
+    ctx.fillStyle();
+    // ctx.lineTo(x, y);
+    // ctx.stroke();
+  };
+  canvasBasic.onmouseup = function () {
+    canvasBasic.onmousemove = null;
+  };
+};
 // });
